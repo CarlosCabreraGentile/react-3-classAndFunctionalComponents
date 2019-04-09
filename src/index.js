7bad547fa13847504a20c58a98b5a16ae7a92f13
Carlos Cabrera <ccabrera@beerealit.com>
2019-04-09 16:09:36 -0300

    create and initialize state

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+  //With the constructor we initialize the state
+  constructor(props) {
+    //super function it´s related with the React.Component base class
+    super(props);
+  }
+
+  //React says we have to define render!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       position => console.log(position),
