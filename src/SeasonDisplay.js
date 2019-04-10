47f53b56c88462d14db0fe84b3315e7f4426f664
Carlos Cabrera <ccabrera@beerealit.com>
2019-04-10 11:29:30 -0300

    get result of season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
 
-const SeasonDisplay = () => {
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    console.log(month);
+
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = props => {
+  let month = new Date().getMonth();
+  const season = getSeason(props.lat, month);
+  console.log(season);
+
   return <div>Season Display</div>;
 };
 
